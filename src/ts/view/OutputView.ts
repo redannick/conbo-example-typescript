de9f1dd0dd0c49becd15348d16a342e392acbf24
Neil Rackett <neil.rackett@mesmotronic.com>
2017-11-27 17:45:37 +0000

    Updated decorators

--- src/ts/view/OutputView.ts
+++ src/ts/view/OutputView.ts
@@ -1,17 +1,14 @@
-import { View, Viewable } from 'conbo';
+import { View, Viewable, Injectable } from 'conbo';
 
 import MyModel from '../model/MyModel';
 
 @Viewable('conbo.example.typescript', 'OutputView')
 export default class OutputView extends View
 {
-	public myModel:MyModel;
+	@Injectable public myModel:MyModel;
 
 	protected declarations()
 	{
-		// Automatically injected
-		this.myModel = undefined;
-
 		this.tagName = 'h1';
 		this.template = `Hello, {{myModel.name|parseName}}!`;
 	}
