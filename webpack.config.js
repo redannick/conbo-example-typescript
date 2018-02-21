a9b345bf87a6a9464667a2d9968928365b9fb4e6
Neil Rackett <neil.rackett@mesmotronic.com>
2018-02-21 10:41:46 +0000

    Updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,7 @@ module.exports =
 	output: 
 	{
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'TypeScriptExample.js',
-        publicPath: 'http://localhost:8086/'
+		filename: 'TypeScriptExample.js'
     },
 
 	resolve: 
