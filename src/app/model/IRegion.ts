e614eb8ae70d4bf7a9237aad96b20d2a69f5d702
Robin Wilding <robin@redannick.com>
2018-12-07 16:27:00 +0000

    works on conbo 4.2.5 but not on 4.3.14. runtime errors

---
+++ src/app/model/IRegion.ts
@@ -0,0 +1,6 @@
+import ILocation from './ILocation';
+export default interface IRegion {
+	i:number;
+	name:string;
+	locations:Array<ILocation>;
+}
--- src/app/model/MyModel.ts
+++ src/app/model/MyModel.ts
@@ -1,6 +1,15 @@
 import { Hash } from 'conbo';
+import IRegion from './IRegion';
 
 export default class MyModel extends Hash
 {
 	public name:string = 'Conbo';
+	_regions: any;
+	declarations() {
+		// this._regions = new IRegion();
+	}
+	public get regionList ():Array<string>{
+		let list:Array<string> = this._regions.map((region:IRegion)=>region.name);
+		return list;
+	}
 }
